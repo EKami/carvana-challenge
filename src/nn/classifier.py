c22956e9d26b168d77d33d464a8559aed54c145e
Tuatini Godard <tuatinigodard@gmail.com>
2017-08-28 02:13:46 +0200

    Add validation set verification

--- src/nn/classifier.py
+++ src/nn/classifier.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader
 import nn.tools as tools
 
-import nn.losses as losses
+import nn.losses as losses_utils
 import numpy as np
 
 
@@ -18,53 +18,104 @@ class CarvanaClassifier:
 
     def _criterion(self, logits, labels):
         # l = BCELoss2d()(logits, labels)
-        l = losses.BCELoss2d().forward(logits, labels) + losses.SoftDiceLoss().forward(logits, labels)
+        l = losses_utils.BCELoss2d().forward(logits, labels) + losses_utils.SoftDiceLoss().forward(logits, labels)
         return l
 
-    def _evaluate_predictions(self, threshold):
+    def _validate_epoch(self, threshold):
         valid_dataset = self.valid_loader.dataset
 
         num = len(valid_dataset)
         height, width = self.train_loader.dataset.img_resize
         predictions = np.zeros((num, 2 * height, 2 * width), np.float32)
+        losses = tools.AverageMeter()
+        accuracies = tools.AverageMeter()
 
         test_acc = 0
         test_loss = 0
-        test_num = 0
-        #for ind, (inputs, masks) in enumerate(self.train_loader, 0):
-        # for ind, (images, masks, indices) in enumerate(self.valid_loader, 0):
-        #     if self.use_cuda:
-        #         images = images.cuda()
-        #         masks = masks.cuda()
-        #
-        #     images = Variable(images, volatile=True)
-        #     masks = Variable(masks, volatile=True)
-        #
-        #     # forward
-        #     logits = self.net(images)
-        #     probs = F.sigmoid(logits)
-        #     pred = (probs > threshold).float()
-        #
-        #     loss = self._criterion(logits, masks)
-        #     acc = losses.dice_loss(pred, masks)
-        #
-        #     batch_size = len(indices)
-        #     test_num += batch_size
-        #     test_loss += batch_size * loss.data[0]
-        #     test_acc += batch_size * acc.data[0]
-        #     start = test_num - batch_size
-        #     end = test_num
-        #     predictions[start:end] = probs.data.cpu().numpy().reshape(-1, 2 * height, 2 * width)
-        #
+        for ind, (images, target) in enumerate(self.valid_loader, 0):
+            if self.use_cuda:
+                images = images.cuda()
+                target = target.cuda()
+
+            images = Variable(images, volatile=True)
+            target = Variable(target, volatile=True)
+            batch_size = images.size(0)
+
+            # forward
+            logits = self.net(images)
+            probs = F.sigmoid(logits)
+            pred = (probs > threshold).float()
+
+            loss = self._criterion(logits, target)
+            acc = losses_utils.dice_loss(pred, target)
+            losses.update(loss.data[0], batch_size)
+            accuracies.update(acc.data[0], batch_size)
+
+            # batch_size = len(indices)
+            # test_num += batch_size
+            # test_loss += batch_size * loss.data[0]
+            # test_acc += batch_size * acc.data[0]
+            # start = test_num - batch_size
+            # end = test_num
+            # predictions[start:end] = probs.data.cpu().numpy().reshape(-1, 2 * height, 2 * width)
+
         # assert (test_num == len(self.valid_loader.sampler))
         #
         # test_loss = test_loss / test_num
         # test_acc = test_acc / test_num
 
-        return predictions, test_loss, test_acc
+        return losses.avg, accuracies.avg
+
+    def _train_epoch(self, epoch_id, optimizer, threshold, it_smooth, it_print):
+
+        # TODO Replace these by tools.AverageMeter()
+        sum_smooth_loss = 0.0
+        sum_smooth_acc = 0.0
+        sum = 0
+        num_its = len(self.train_loader)
+        for ind, (inputs, target) in enumerate(self.train_loader, 0):
+
+            if self.use_cuda:
+                inputs = inputs.cuda()
+                target = target.cuda()
+            inputs, target = Variable(inputs), Variable(target)
+
+            # forward
+            logits = self.net.forward(inputs)
+            probs = F.sigmoid(logits)
+            pred = (probs > threshold).float()
+
+            # backward + optimize
+            loss = self._criterion(logits, target)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # print statistics
+            acc = losses_utils.dice_loss(pred, target)
+            lr = tools.get_learning_rate(optimizer)[0]
+
+            sum_smooth_loss += loss.data[0]
+            sum_smooth_acc += acc.data[0]
+            sum += 1
+
+            if ind % it_smooth == 0:
+                smooth_loss = sum_smooth_loss / sum
+                smooth_acc = sum_smooth_acc / sum
+                sum_smooth_loss = 0.0
+                sum_smooth_acc = 0.0
+                sum = 0
+
+            if ind % it_print == 0 or ind == num_its - 1:
+                train_acc = acc.data[0]
+                train_loss = loss.data[0]
+                print("Epochs {}, batch = {}, train_loss= {}, train_acc = {}, "
+                      .format(epoch_id, ind, train_loss, train_acc))
+                # print('\r%5.1f   %5d    %0.4f   |  %0.4f  %0.4f | %0.4f  %6.4f | ... ' %
+                #       (epoch + (ind + 1) / num_its, ind + 1, lr, smooth_loss, smooth_acc, train_loss, train_acc),
+                #       end='', flush=True)
 
     def train(self, epochs, threshold=0.5):
-        self.net.train()
 
         if self.use_cuda:
             self.net.cuda()
@@ -77,56 +128,17 @@ class CarvanaClassifier:
         it_print = 1
         it_smooth = 20
 
-        for i_epoch, epoch in enumerate(range(epochs)):
+        for epoch_id, epoch in enumerate(range(epochs)):
+            self.net.train()
 
-            sum_smooth_loss = 0.0
-            sum_smooth_acc = 0.0
-            sum = 0
-            num_its = len(self.train_loader)
-            for ind, (inputs, masks) in enumerate(self.train_loader, 0):
+            # Run a train pass on the current epoch
+            self._train_epoch(epoch_id, optimizer, threshold, it_smooth, it_print)
 
-                if self.use_cuda:
-                    inputs = inputs.cuda()
-                    masks = masks.cuda()
-                inputs, masks = Variable(inputs), Variable(masks)
+            # switch to evaluate mode
+            self.net.eval()
 
-                # forward
-                logits = self.net.forward(inputs)
-                probs = F.sigmoid(logits)
-                pred = (probs > threshold).float()
-
-                # backward + optimize
-                loss = self._criterion(logits, masks)
-                optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-
-                # print statistics
-                acc = losses.dice_loss(pred, masks)
-                lr = tools.get_learning_rate(optimizer)[0]
-
-                sum_smooth_loss += loss.data[0]
-                sum_smooth_acc += acc.data[0]
-                sum += 1
-
-                if ind % it_smooth == 0:
-                    smooth_loss = sum_smooth_loss / sum
-                    smooth_acc = sum_smooth_acc / sum
-                    sum_smooth_loss = 0.0
-                    sum_smooth_acc = 0.0
-                    sum = 0
-
-                valid_predictions, valid_loss, valid_acc = self._evaluate_predictions(threshold)
-
-                if ind % it_print == 0 or ind == num_its - 1:
-                    train_acc = acc.data[0]
-                    train_loss = loss.data[0]
-                    print("Epochs {}, batch = {}, train_loss= {}, train_acc = {}, "
-                          "valid_loss = {} valid_acc = {}".
-                          format(i_epoch, ind, train_loss, train_acc, valid_loss, valid_acc))
-                    # print('\r%5.1f   %5d    %0.4f   |  %0.4f  %0.4f | %0.4f  %6.4f | ... ' %
-                    #       (epoch + (ind + 1) / num_its, ind + 1, lr, smooth_loss, smooth_acc, train_loss, train_acc),
-                    #       end='', flush=True)
+            valid_loss, valid_acc = self._validate_epoch(threshold)
+            print("Validation loss = {}, Validation acc = {}".format(valid_loss, valid_acc))
 
     def predict(self, test_loader):
         self.net.eval()
--- src/nn/tools.py
+++ src/nn/tools.py
@@ -1,5 +1,7 @@
-# net ------------------------------------
+
+
 # https://github.com/pytorch/examples/blob/master/imagenet/main.py ###############
+# https://github.com/pytorch/examples/blob/master/imagenet/main.py#L291
 def adjust_learning_rate(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
@@ -10,3 +12,21 @@ def get_learning_rate(optimizer):
     for param_group in optimizer.param_groups:
         lr += [param_group['lr']]
     return lr
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count

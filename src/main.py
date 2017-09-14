59c095b0013070f0b1a8a7d54ecfa9375a7ba8f0
Tuatini Godard <tuatinigodard@gmail.com>
2017-09-14 15:02:52 +0200

    Add model persistence

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import nn.classifier
 import nn.unet as unet
-import utils
+import helpers
 
 import torch
 from torch.utils.data import DataLoader
@@ -22,13 +22,13 @@ import img.transformer as transformer
 
 
 def main():
-    # Clear output dirs first
-    utils.clear_output_dirs()
+    # Clear log dir first
+    helpers.clear_logs_folder()
 
     # Hyperparameters
     img_resize = (1024, 1024)
     batch_size = 3
-    epochs = 200
+    epochs = 100
     threshold = 0.5
     n_fold = 5
 
@@ -39,7 +39,7 @@ def main():
     tb_viz_cb = TensorboardVisualizerCallback(os.path.join(script_dir, '../logs/tb_viz'))
     tb_logs_cb = TensorboardLoggerCallback(os.path.join(script_dir, '../logs/tb_logs'))
     kf = KFold(n_splits=n_fold, shuffle=True)
-    sample_size = 0.2 # None  # Put None to work on full dataset
+    sample_size = 0.2  # None  # Put None to work on full dataset
 
     # Download the datasets
     ds_tools = DatasetTools()
@@ -59,9 +59,11 @@ def main():
     epochs_per_fold = np.maximum(1, np.round(epochs / n_fold).astype(int))
 
     # Define our nn architecture
-    #net = unet.UNet128((3, *img_resize))
+    # net = unet.UNet128((3, *img_resize))
     net = unet.UNet1024((3, *img_resize_centercrop))
-    classifier = nn.classifier.CarvanaClassifier(net, epochs_per_fold * n_fold)
+    classifier = nn.classifier.CarvanaClassifier(net, epochs_per_fold * n_fold,
+                                                 os.path.join(script_dir, '../models/model_' +
+                                                              helpers.get_model_timestamp()))
 
     # Launch the training on k folds
     for i, (train_indexes, valid_indexes) in enumerate(kf.split(full_x_train)):
@@ -86,7 +88,10 @@ def main():
             print("Training on {} samples and validating on {} samples "
                   .format(len(train_loader.dataset), len(valid_loader.dataset)))
 
-        classifier.train(train_loader, valid_loader, epochs_per_fold, callbacks=[tb_viz_cb, tb_logs_cb])
+        pth = classifier.train(train_loader, valid_loader, epochs_per_fold,
+                               callbacks=[tb_viz_cb, tb_logs_cb], train_pass_name="kfold_" + str(i+1))
+        print("KFold {} finished. Model saved in {}".format(str(i+1), pth))
+        classifier.restore_model(pth)
 
     test_ds = TestImageDataset(full_x_test, img_resize)
     test_loader = DataLoader(test_ds, batch_size,
@@ -98,7 +103,7 @@ def main():
     output_file = os.path.join(script_dir, '../output/submit.csv.gz')
     classifier.predict(test_loader, to_file=output_file,
                        t_fnc=saver.get_prediction_transformer,
-                       fnc_args=[origin_img_size, 0.5])  # The get_prediction_transformer arguments
+                       fnc_args=[origin_img_size, 0.5])
 
 
 if __name__ == "__main__":

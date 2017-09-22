e03ccab14970530adca4684fa09dfb3616d572b1
Tuatini Godard <tuatinigodard@gmail.com>
2017-09-22 16:01:57 +0200

    Replace accuracy metric by dice coeff

--- src/nn/losses.py
+++ src/nn/losses.py
@@ -2,26 +2,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class CrossEntropyLoss2d(nn.Module):
-    def __init__(self, weight=None, size_average=True):
-        super(CrossEntropyLoss2d, self).__init__()
-        self.nll_loss = nn.NLLLoss2d(weight, size_average)
-
-    def forward(self, logits, targets):
-        return self.nll_loss(F.log_softmax(logits), targets)
-
-
-# https://github.com/bermanmaxim/jaccardSegment/blob/master/losses.py
-class StableBCELoss(nn.Module):
-    def __init__(self):
-        super(StableBCELoss, self).__init__()
-
-    def forward(self, input, target):
-        neg_abs = - input.abs()
-        loss = input.clamp(min=0) - input * target + (1 + neg_abs.exp()).log()
-        return loss.mean()
-
-
 class BCELoss2d(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(BCELoss2d, self).__init__()
@@ -51,12 +31,11 @@ class SoftDiceLoss(nn.Module):
 
 
 # https://github.com/pytorch/pytorch/issues/1249
-def dice_loss(m1, m2):
-    num = m1.size(0)
-    m1 = m1.view(num, -1)
-    m2 = m2.view(num, -1)
-    intersection = (m1 * m2)
+def dice_coeff(pred, target):
+    smooth = 1.
+    num = pred.size(0)
+    m1 = pred.view(num, -1)  # Flatten
+    m2 = target.view(num, -1)  # Flatten
+    intersection = (m1 * m2).sum()
 
-    score = 2. * (intersection.sum(1) + 1) / (m1.sum(1) + m2.sum(1) + 1)
-    score = score.sum() / num
-    return score
+    return (2. * intersection + smooth) / (m1.sum() + m2.sum() + smooth)
